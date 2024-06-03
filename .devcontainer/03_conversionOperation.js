1abf1f630378854b6462ad6304f66ac93c63b7e9
Mahima Jindal <mahimajindalrke@gmail.com>
2024-06-03 15:47:08 +0000

    conversions

---
+++ .devcontainer/03_conversionOperation.js
@@ -0,0 +1,31 @@
+/*let score = undefined
+
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)*/
+
+//if value of score is "33" => we get o/p=33 only after converting in number
+// if value of score is "33abc" => we get o/p=NaN and converted type is still number
+// if score value is null =>type of null= object, o/p = 0, convertedType=number
+//if score is undefined =>type of score=undefined, o/p=NaN, same
+//if score is true => o/p=1
+//if score is "hitesh" => o/p is NaN
+
+//-----------------------------------converting to boolean--------------------//
+/*let isLoggedIn ="mahi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)*/
+//1 => o/p is true
+//""=> false
+//"mahi"=> true
+
+//----------------------------converting  to String--------------------------//
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+
+
+//44 => 44
